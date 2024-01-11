f3a94518a4957c834f470ac401a27bdaa55c4954
Tinoco <ticaps@ticapsoriginal.com>
2024-01-11 12:29:04 -0300

    Step 2 define runtime

--- infra/physical/closeprocess.py
+++ infra/physical/closeprocess.py
@@ -1,10 +1,14 @@
+import time
 import os
 import psutil
 
 print(psutil.pids())
-for item in (psutil.pids()):
-    name = psutil.Process(int(item))
-    print(str(item) + "    " + str(name.name))
-    if "Macs Fan Control" in str(name.name):
-        name.terminate()
-        name.wait()
+for i in range(1000):
+    j = psutil.pids()
+    for item in (j):
+        name = psutil.Process(int(item))
+        print(str(item) + "    " + str(name.name))
+        if "Macs Fan Control" in str(name.name):
+            name.terminate()
+            name.wait()
+    time.sleep(5)
